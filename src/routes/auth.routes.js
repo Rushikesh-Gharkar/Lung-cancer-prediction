adf84947cb8c9b718e9a9e392ccdf22a072cb9f2
rus.ggs <rushikeshgharkar31@gmail.com>
2026-02-07 23:52:04 +0530

    auth routes and validators added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const {registerUser , userLogin} = require('../controllers/authController')
 const {registerSchema , loginSchema} = require('../validators/auth.schema')
+const {validate} = require('../middleware/validate')
 
+const router = express.Router()
 
+router.post("/register" , validate(registerSchema) , registerUser)
+router.post("/login" , validate(loginSchema) , userLogin)
+
+module.exports = {
+    router:router
+}
